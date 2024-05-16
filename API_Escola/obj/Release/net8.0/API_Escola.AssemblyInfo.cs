bcead126da154314807ed4d3d49990c1750fa328
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2024-05-16 13:53:05 -0300

    feat: criando o model

--- API_Escola/obj/Release/net8.0/API_Escola.AssemblyInfo.cs
+++ API_Escola/obj/Release/net8.0/API_Escola.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Escola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34e15906238d287c00227308be864c4294c5702")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Escola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Escola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
